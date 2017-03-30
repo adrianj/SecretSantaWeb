93abb5f908abfac1da5ce700d7cafa83f6826a08
Adrian Jongenelen <jongy10@hotmail.com>
2017-03-30 20:39:03 +1300

    Created Family, Person and FamiliesController.

---
+++ SecretSantaWeb/Models/Family.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SecretSantaWeb.Models
+{
+    public class Family
+    {
+        public int FamilyID { get; set; }
+        public string FamilyName { get; set; }
+
+        public virtual List<Person> FamilyMembers { get; set; }
+
+    }
+}
---
+++ SecretSantaWeb/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace SecretSantaWeb.Models
+{
+    public class Person
+    {
+        public int PersonID { get; set; }
+
+        public string Name { get; set; }
+
+        public int FamilyID { get; set; }
+        public virtual Family Family { get; set; }
+
+    }
+}
