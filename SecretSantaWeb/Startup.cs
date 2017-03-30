8b03b6d45ad6548212c8b6cf3ee97e787681509a
Adrian Jongenelen <jongy10@hotmail.com>
2017-03-30 20:15:49 +1300

    Initial creation of ASP.NET Web App

---
+++ SecretSantaWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SecretSantaWeb.Startup))]
+namespace SecretSantaWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
